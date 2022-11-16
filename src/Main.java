0fede3d4e781c61a7a4857fe4a9933bfff90febb
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-16 20:31:44 +0300

    ДЗ Массивы ч.1 Задание 2

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,39 @@ public class Main {
         double[] number1 = {1.57, 7.654, 9.986};
 
         char[] number2 = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+        // Задание 2
+        System.out.println("Задание 2");
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        double[] arr1 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i != arr1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        char[] arr2 = {'a', 'b', 'c', 'd', 'e', 'f'};
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+
     }
 }
