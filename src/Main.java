fb53ea77b394b88bf5584f96c0859e2100106e85
ITevgeniydav2022 <defox2022@gmail.com>
2022-11-16 23:19:26 +0300

    ДЗ Массивы ч.1 Задание 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+
         // Задание 1
         System.out.println("Задание 1");
         int[] number = new int[3];
